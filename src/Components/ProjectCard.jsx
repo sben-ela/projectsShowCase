59c466f5f9db60ad7c2d25feef5bb21ac0f5d0a5
SalahEddine Ben Elarradia <sben-ela@github.com>
2024-10-22 22:53:01 +0100

    test

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -7,9 +7,7 @@ import {
 } from "framer-motion";
 
 const ROTATION_RANGE = 60;
-const HALF_ROTATION_RANGE = 60 / 2;
-
-
+const HALF_ROTATION_RANGE = ROTATION_RANGE / 2;
 
 const ProjectCard = ({ title, image, onSeeMoreClick }) => {
   const ref = useRef(null);
@@ -23,7 +21,7 @@ const ProjectCard = ({ title, image, onSeeMoreClick }) => {
   const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
 
   const handleMouseMove = (e) => {
-    if (!ref.current) return [0, 0];
+    if (!ref.current) return;
 
     const rect = ref.current.getBoundingClientRect();
 
@@ -53,13 +51,12 @@ const ProjectCard = ({ title, image, onSeeMoreClick }) => {
       style={{
         transformStyle: "preserve-3d",
         transform,
-        backgroundImage: `url(${image})`, // Add your image URL here
-        backgroundSize: "cover", // Make sure the image covers the entire card
+        backgroundImage: `url(${image})`, // Corrected syntax for backgroundImage
+        backgroundSize: "cover", // Ensure the image covers the entire card
         backgroundPosition: "center", // Center the background image
       }}
-      className="relative h-96 w-72 rounded-xl shadow-lg overflow-hidden transition-all duration-300">
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-80 z-0"></div>
-
+      className="relative bg-black h-96 w-72 rounded-xl shadow-lg"
+    >
       <div
         style={{
           transform: "translateZ(75px)",
@@ -67,30 +64,25 @@ const ProjectCard = ({ title, image, onSeeMoreClick }) => {
         }}
         className="absolute group space-y-10 inset-4 grid place-content-center rounded-xl"
       >
-        {/* <div className="absolute h-20 inset-0 bg-slate-700 bg-opacity-80 blur-lg rounded-xl"></div> */}
+        <div className="absolute h-full w-full inset-0 bg-slate-700 bg-opacity-40 blur-lg rounded-xl"></div>
 
         <p
-          style={{ transform: "translateZ(40px)" }}
-          className="text-white text-4xl font-bold tracking-wider text-center"
+          style={{
+            transform: "translateZ(50px)",
+          }}
+          className="absolute right-0 z-10 p-4 -top-10 mr-8 rounded-xl text-4xl font-bold text-gray-100"
         >
           {title}
         </p>
 
         <div className="absolute bottom-0 w-full flex justify-center items-center opacity-0 translate-y-20 group-hover:opacity-100 group-hover:-translate-y-10 transition-transform duration-500 ease-in-out">
-        <a
-      onClick={onSeeMoreClick}
-      className="border-2 w-40 font-serif text-center cursor-pointer bg-gray-400 text-xl text-black border-gray-600 p-2 rounded-2xl relative
-        transition-all duration-500 ease-in-out hover:bg-gradient-to-tr via-gray-400 from-blue to-black
-        shadow-md hover:shadow-xl
-        hover:scale-105
-      "
-        >
-          See More
-        </a>
-
-
+          <a
+            onClick={onSeeMoreClick}
+            className="border-2 w-40 font-serif text-center cursor-pointer group-hover:bg-gradient-to-tr hover:via-slate-300 from-slate-400 to-stone-800 bg-slate-200 text-xl border-black p-2 rounded-2xl"
+          >
+            See More
+          </a>
         </div>
-
       </div>
     </motion.div>
   );
